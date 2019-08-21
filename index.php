48a86bf680aff12a0bb24cc5278204aab2d20e02
farleydsantana <54377247+farleydsantana@users.noreply.github.com>
2019-08-21 18:50:35 -0300

    resolvido 2ª conflito

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 
-<<<<<<< HEAD
+
 echo "Ola, mundo! argentina";
 echo "boa noite";
-=======
-echo "Ola, mundo brasil!";
-echo "seja bem-vindo";
->>>>>>> 6c0a8d6c4d5a0df99f491ce74a6770662fd054c6
+
 ?>
