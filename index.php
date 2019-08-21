9be917ff7d5c67974037c67de0827679d8f23940
Farley Duraes <farley_duraes@hotmail.com>
2019-08-21 18:54:37 -0300

    mudei o primeiro echo e adcionei o segundo echo

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
-<?php
+<? php
 
-<<<<<<< HEAD
-echo "Ola, mundo! argentina";
-echo "boa noite";
-=======
-echo "Ola, mundo brasil!";
-echo "seja bem-vindo";
->>>>>>> 6c0a8d6c4d5a0df99f491ce74a6770662fd054c6
-?>
+
+eco  " Ola, mundo! argentina " ;
+eco  " boa noite " ;
+
+? >
